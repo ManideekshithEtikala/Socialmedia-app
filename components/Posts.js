eb05e82a07c803fb64765865b61792f2dd4ff55f
RakshithaDugyala <manideekshith@gmail.com>
2024-07-27 22:39:05 +0530

    small changes completed

--- components/Posts.js
+++ components/Posts.js
@@ -5,29 +5,29 @@ import React from "react";
 export default function Posts({ post }) {
   return (
     <main className="w-full flex px-3 py-5 border-b">
-      <div className="w-1/5">
+      <div className="w-fit ">
         <img src={post.userimg} className="w-12 h-12 rounded-full mr-4 cursor-pointer" />
       </div>
-      <div className="w-4/5">
+      <div className="w-full">
         <div className="flex justify-between w-full">
-          <div className="flex pr-1 items-center py-2">
-            <h1 className="font-bold text-xl text-gray-800 pr-1 font-serif hover:underline hover:cursor-pointer">{post.name}</h1>
+          <div className="flex items-center py-2">
+            <h1 className="font-bold text-xl text-gray-800 font-serif hover:underline hover:cursor-pointer">{post.name}</h1>
             <p className="font-medium text-gray-700 px-1 hover:cursor-pointer">{post.username}</p>
             <span className="text-gray-400 text-sm px-1">{post.timestamp}</span>
           </div>
           <div>
-            <DotsCircleHorizontalIcon className="w-7 h-7 cursor-pointer text-gray-700" />
+            <DotsCircleHorizontalIcon className="w-7 h-7 cursor-pointer text-gray-700  hover:text-gray-900" />
           </div>
         </div>
         <div className="text-gray-800 pb-2">{post.text}</div>
         <div className="w-full h-52">
-            <img src={post.img} alt="posting image" className="w-full h-full rounded-lg"/>
+            <img src={post.img} alt="posting image" className="w-fit h-full rounded-lg"/>
         </div>
         <div className="flex justify-between w-full mt-3 pt-2 text-gray-600">
-            <HeartIcon className="w-8 cursor-pointer"/>
-            <ChatIcon className="w-8 cursor-pointer"/>
-            <ShareIcon className="w-8 cursor-pointer"/>
-            <SaveAsIcon className="w-8 cursor-pointer"/>
+            <HeartIcon className="w-8 cursor-pointer hover:text-gray-900"/>
+            <ChatIcon className="w-8 cursor-pointer hover:text-gray-900"/>
+            <ShareIcon className="w-8 cursor-pointer hover:text-gray-900"/>
+            <SaveAsIcon className="w-8 cursor-pointer hover:text-gray-900"/>
         </div>
       </div>
     </main>
