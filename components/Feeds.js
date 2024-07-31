451fe602cdc30ac3b824cf686c9cf338d69d5bad
RakshithaDugyala <manideekshith@gmail.com>
2024-08-01 01:04:41 +0530

    showing the posted images and data in post seciton

--- components/Feeds.js
+++ components/Feeds.js
@@ -1,39 +1,19 @@
 import { OfficeBuildingIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import MsgInputs from './MsgInputs'
 import Posts from './Posts'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../firebase'
+
 
 export default function Feeds() {
-
-  const posts=[
-    {
-      id:1,
-      name:"Manidekshith",
-      username:"mani@143",
-      userimg:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      img:"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-      text:"Picture loks great",
-      timestamp:"3 hours ago",
-    },
-    {
-      id:2,
-      name:"manidekshith",
-      username:"mani@143",
-      userimg:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      img:"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-      text:"Picture loks great",
-      timestamp:"3 hours ago",
-    },
-    {
-      id:3,
-      name:"manidekshith",
-      username:"mani@143",
-      userimg:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
-      img:"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",
-      text:"Picture loks great",
-      timestamp:"3 hours ago",
-    }
-  ]
+const [posts,setPosts] = useState([])
+useEffect(()=>onSnapshot(
+  query(collection(db,"posts"),orderBy("timestamp","desc")),
+  (snapshot)=>{
+    setPosts(snapshot.docs)
+  }
+),[])
   return (
     <div className='xl:ml-[350px] border-l border-r xl:min-w-[550px]'>
         <div className='flex justify-between px-2 my-3 border-b pb-2 font-extrabold sticky'>
--- components/Posts.js
+++ components/Posts.js
@@ -1,27 +1,28 @@
 import { ChatIcon, HeartIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/outline";
 import { DotsCircleHorizontalIcon, SaveAsIcon } from "@heroicons/react/solid";
 import React from "react";
+import Moment from "react-moment";
 
 export default function Posts({ post }) {
   return (
     <main className="w-full flex px-3 py-5 border-b">
       <div className="w-fit ">
-        <img src={post.userimg} className="w-12 h-12 rounded-full mr-4 cursor-pointer" />
+        <img src={post.data().userimg} className="w-12 h-12 rounded-full mr-4 cursor-pointer" />
       </div>
       <div className="w-full">
         <div className="flex justify-between w-full">
           <div className="flex items-center py-2">
             <h1 className="font-bold text-xl text-gray-800 font-serif hover:underline hover:cursor-pointer">{post.name}</h1>
-            <p className="font-medium text-gray-700 px-1 hover:cursor-pointer">{post.username}</p>
-            <span className="text-gray-400 text-sm px-1">{post.timestamp}</span>
+            <p className="font-medium text-gray-700 px-1 hover:cursor-pointer hover:underline">{post.data().username}</p>
+            <span className="text-gray-400 text-sm px-1"><Moment fromNow>{post?.timestamp?.toDate()}</Moment></span>
           </div>
-          <div>
+           <div>
             <DotsCircleHorizontalIcon className="w-7 h-7 cursor-pointer text-gray-700  hover:text-gray-900" />
           </div>
         </div>
-        <div className="text-gray-800 pb-2">{post.text}</div>
+        <div className="text-gray-800 pb-2">{post.data().text}</div>
         <div className="w-full h-52">
-            <img src={post.img} alt="posting image" className="w-fit h-full rounded-lg"/>
+            <img src={post.data().image} alt="posting image" className="w-fit h-full rounded-lg"/>
         </div>
         <div className="flex justify-between w-full mt-3 pt-2 text-gray-600">
             <HeartIcon className="w-8 cursor-pointer hover:text-gray-900"/>
