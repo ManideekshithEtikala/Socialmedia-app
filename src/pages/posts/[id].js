af38ae33879c82513e2e6787b01170bc0d6821c3
RakshithaDugyala <manideekshith@gmail.com>
2024-08-02 21:35:44 +0530

    finally we get the total comments to the comment page from database

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -6,19 +6,41 @@ import Component from "../../../components/Component";
 import { ArrowLeftIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { onSnapshot,doc } from "firebase/firestore";
+import {
+  onSnapshot,
+  doc,
+  collection,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../../../firebase";
+import Comment from "../../../components/Comment";
 // Initialize font
 const inter = Inter({ subsets: ["latin"] });
 
 // Define the main component
 export default function Post({ newsResult, randomUsers }) {
-    const router=useRouter()
-    const {id}=router.query
-    const [post,setPost]=useState()
-    useEffect(()=>{
-        onSnapshot(doc(db,"posts",id),(snapshot)=>setPost(snapshot))
-    },[db,id])
+  const router = useRouter();
+  const { id } = router.query;
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
+  //get post data
+  useEffect(() => {
+    onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot));
+  }, [db, id]);
+
+  //get comments data
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comment"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
   return (
     <div className="flex min-h-screen max-w-7xl mx-auto ">
       {/* Sidebar */}
@@ -27,11 +49,25 @@ export default function Post({ newsResult, randomUsers }) {
       <div className="xl:ml-[350px] border-l border-r xl:min-w-[550px]">
         <div className="flex px-3 my-3 border-b pb-2 font-extrabold sticky items-center">
           <div>
-            <ArrowLeftIcon onClick={()=>router.push("/")} className="h-10 p-2 hover:bg-gray-100 rounded-full text-black flex items-center justify-center" />
+            <ArrowLeftIcon
+              onClick={() => router.push("/")}
+              className="h-10 p-2 hover:bg-gray-100 rounded-full text-black flex items-center justify-center"
+            />
           </div>
           <h1>Home</h1>
         </div>
-      <Posts id={id} post={post}/>
+        <Posts id={id} post={post} />
+        {comments.length > 0 && (
+          <div className="">
+            {comments.map((comment) => (
+              <Comment
+                comment={comment.data()}
+                key={comment.id}
+                id={comment.id}
+              />
+            ))}
+          </div>
+        )}
       </div>
       {/* widgets */}
       <Widgets
